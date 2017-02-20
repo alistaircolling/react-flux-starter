167bd24097bbed2ca121063477afabe15544b0bf
Alistair Colling <alistair.colling@gmail.com>
2017-02-20 19:00:18 +0000

    Adding in eslint files

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+             render: function() {
+        return (
+
+            <div className="jumbotron">
+                <h1>The About Page</h1>
+                <p>This is the about page</p>
+            </div>
+        );
+    }
+});
+
+module.exports = About;
